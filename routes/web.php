3b029b0508f7711d27753914f76e3331b3aeba65
usc-kmr041 <kmr041@student.usc.edu.au>
2022-10-27 12:07:36 +1000

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [TodoController::class, 'index']);
+
+Route::get('create', [TodoController::class, 'create']);
+
+Route::get('details/{todo}', [TodoController::class, 'details']);
+
+Route::get('delete/{todo}', [TodoController::class, 'delete']);
+
+Route::post('store-data', [TodoController::class, 'store']);
+
+Route::get('/edit/{todo}', [TodoController::class, 'edit']);
+
+Route::post('/update/{todo}', [TodoController::class,'update']);
